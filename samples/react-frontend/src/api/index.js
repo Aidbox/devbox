35cf2c8e850ee829b25c892575c5df267688d9ac
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2018-08-18 13:05:05 +0300

    cleanup things

--- samples/react-frontend/src/api/index.js
+++ samples/react-frontend/src/api/index.js
@@ -1,50 +1,64 @@
-import axios from 'axios'
-import createDebug from 'debug'
+import axios from "axios";
+import createDebug from "debug";
 
-const debug = createDebug('ehr:api') // eslint-disable-line no-unused-vars
+const debug = createDebug("ehr:api"); // eslint-disable-line no-unused-vars
 
 const myAxios = axios.create({
-    headers: {'Authorization': 'Basic d2VubG9naWM6d2VubG9naWNwYXNzd29yZA=='},
-  baseURL: 'https://wenlogic.aidbox.app/',
-})
+  baseURL: process.env["AIDBOX_URL"]
+});
 
-if (process.env.NODE_ENV === 'development') { // искусственная задержка для development
-  myAxios.interceptors.request.use(function (config) {
-    return Promise.delay(100).then(function () { return (config) })
-  }, function (error) {
-    return Promise.reject(error)
-  })
+if (process.env.NODE_ENV === "development") {
+  // slow down requests for dev environment
+  myAxios.interceptors.request.use(
+    function(config) {
+      return Promise.delay(100).then(function() {
+        return config;
+      });
+    },
+    function(error) {
+      return Promise.reject(error);
+    }
+  );
 }
 
-export const getPath = function (path) {
-  return myAxios.get(path).then(function (response) { return response.data })
-}
+export const getPath = function(path) {
+  return myAxios.get(path).then(function(response) {
+    return response.data;
+  });
+};
 
-export const getAllPatients = function ({page}) {
-    return myAxios.get(`/fhir/Patient?_page=${page}&_totalMethod=count`).then(function (response) {
-        return response.data })
-}
+export const getAllPatients = function({ page }) {
+  return myAxios
+    .get(`/fhir/Patient?_page=${page}&_totalMethod=count`)
+    .then(function(response) {
+      return response.data;
+    });
+};
 
-export const getPatientByName = function ({patientName, page}) {
-  return myAxios.get(`/fhir/Patient?name=${patientName}&_page=${page}`).then(function (response) { return response.data })
-}
+export const getPatientByName = function({ patientName, page }) {
+  return myAxios
+    .get(`/fhir/Patient?name=${patientName}&_page=${page}`)
+    .then(function(response) {
+      return response.data;
+    });
+};
 
-const setPatientData = function (values) {
+const setPatientData = function(values) {
   const patientData = {
-    resourceType: 'Patient',
+    resourceType: "Patient",
     name: [
       {
-        given: [ values.firstName ],
-          family:  values.lastName 
+        given: [values.firstName],
+        family: values.lastName
       }
     ],
     address: [
       {
-        use: 'home',
+        use: "home",
         line: values.address.line,
         state: values.address.state,
         city: values.address.city,
-        postalCode: values.address.postalCode,
+        postalCode: values.address.postalCode
       }
     ],
     // identifier: [
@@ -63,28 +77,38 @@ const setPatientData = function (values) {
     //   }
     // ],
     telecom: [
-      ...values.phone.filter(i => i.trim()).map(item => { return {system: 'phone', value: item} }),
-      ...values.email.filter(i => i.trim()).map(item => { return {system: 'email', value: item} }),
-    ],
-  }
-  if (values.birthDate) patientData.birthDate = values.birthDate
-  if (values.gender) patientData.gender = values.gender
-  return patientData
-}
+      ...values.phone.filter(i => i.trim()).map(item => {
+        return { system: "phone", value: item };
+      }),
+      ...values.email.filter(i => i.trim()).map(item => {
+        return { system: "email", value: item };
+      })
+    ]
+  };
+  if (values.birthDate) patientData.birthDate = values.birthDate;
+  if (values.gender) patientData.gender = values.gender;
+  return patientData;
+};
 
-export const putPatient = function (values) {
+export const putPatient = function(values) {
   const data = {
     ...setPatientData(values),
-    id: values.id,
-  }
-  return myAxios.put(`/fhir/Patient/${data.id}`, data).then(function (response) { return response.data })
-}
+    id: values.id
+  };
+  return myAxios.put(`/fhir/Patient/${data.id}`, data).then(function(response) {
+    return response.data;
+  });
+};
 
-export const postPatient = function (values) {
-  const data = setPatientData(values)
-  return myAxios.post('/fhir/Patient', data).then(function (response) { return response.data })
-}
+export const postPatient = function(values) {
+  const data = setPatientData(values);
+  return myAxios.post("/fhir/Patient", data).then(function(response) {
+    return response.data;
+  });
+};
 
-export const deletePatient = function ({patientId}) {
-  return myAxios.delete(`/fhir/Patient/${patientId}`).then(function (response) { return response.data })
-}
+export const deletePatient = function({ patientId }) {
+  return myAxios.delete(`/fhir/Patient/${patientId}`).then(function(response) {
+    return response.data;
+  });
+};
